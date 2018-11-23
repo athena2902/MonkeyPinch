a5530bc73c7a757ea23f2200085cc5bd36e720c4
Quyet Vu <quyetvv_55@vnu.edu.vn>
2018-11-23 08:57:09 +0700

    tap, pinch, pan gesture transform: rotation, scale play chomp

--- MonkeyPinch/ViewController.swift
+++ MonkeyPinch/ViewController.swift
@@ -29,24 +29,85 @@
  */
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-  }
-  
-  @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
+    var chompPlayer:AVAudioPlayer? = nil
     
-  }
-  
-  @IBAction func handleRotate(recognizer : UIRotationGestureRecognizer) {
+    func loadSound(filename: String) -> AVAudioPlayer {
+        let url = Bundle.main.url(forResource: filename, withExtension: "caf")
+        var player = AVAudioPlayer()
+        do {
+            try player = AVAudioPlayer(contentsOf: url!)
+            player.prepareToPlay()
+        } catch {
+            print("Error loading \(url!): \(error.localizedDescription)")
+        }
+        return player
+    }
     
-  }
-  
-  @objc func handleTap(recognizer: UITapGestureRecognizer) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let filteredSubviews = self.view.subviews.filter({
+            $0 is UIImageView })
+        for view in filteredSubviews  {
+            let recognizer = UITapGestureRecognizer(target: self, action:#selector(handleTap(recognizer:)))
+            recognizer.delegate = self
+            view.addGestureRecognizer(recognizer)
+        }
+        self.chompPlayer = self.loadSound(filename: "chomp")
+    }
     
-  }
-  
+    @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
+    
+    @IBAction func handleRotate(recognizer : UIRotationGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.rotated(by: recognizer.rotation)
+            recognizer.rotation = 0
+        }
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        self.chompPlayer?.play()
+    }
+    
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+        
+        if recognizer.state == UIGestureRecognizerState.ended {
+            let velocity = recognizer.velocity(in: self.view)
+            let magnitude = sqrt(velocity.x * velocity.x + velocity.y * velocity.y)
+            let slideMultiplier = magnitude/200
+            print("magnitude: \(magnitude), slideMultiplier: \(slideMultiplier)")
+            
+            let slideFactor = 0.1 * slideMultiplier
+            
+            var finalPoint = CGPoint(x: recognizer.view!.center.x + velocity.x * slideFactor, y: recognizer.view!.center.y + velocity.y * slideFactor)
+            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            UIView.animate(withDuration: Double(slideFactor * 2),
+                           delay: 0,
+                           options: UIViewAnimationOptions.curveEaseOut,
+                           animations: {recognizer.view!.center = finalPoint },
+                           completion: nil)
+        }
+    }
+}
+
+
+extension ViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
 }
